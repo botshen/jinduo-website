ae54b19dd81fb5442100124ba09ffdd2939e5512
Xin shen <363088847@qq.com>
2021-08-20 20:41:07 +0800

    deploy

---
+++ precache-manifest.cec74c4a91d5269991ca1d80c956cdf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1b7126119f1c5737d0",
+    "url": "/jinduo-website/css/app.e3236664.css"
+  },
+  {
+    "revision": "19fdf9e062de478f233b938d76414a19",
+    "url": "/jinduo-website/index.html"
+  },
+  {
+    "revision": "ec1b7126119f1c5737d0",
+    "url": "/jinduo-website/js/app.94a79c69.js"
+  },
+  {
+    "revision": "d5fea481744bb79538bf",
+    "url": "/jinduo-website/js/chunk-vendors.9d9e64fd.js"
+  },
+  {
+    "revision": "a621372b747d30f9eddb25b6a23b329f",
+    "url": "/jinduo-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jinduo-website/robots.txt"
+  }
+]);
